c58d59b33d198e15e78568aa1861b129f0bf6857
Surfsol <surfsol@protonamil.com>
2019-10-18 13:49:55 -0300

    Colorlist MVp

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
-import Login from "./components/Login";
-import "./styles.scss";
+import Login from './components/Login';
+import BubblePage from './components/BubblePage';
+import PrivateRoute from './components/PrivateRoute';
+
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/bubblepage">Bubble Page</Link>
+          </li>
+          
+        </ul>
+        {/* switch, can use without exact */}
+        <Switch>
+          <PrivateRoute path="/bubblepage" component={BubblePage} />
+          <Route path="/login" component={Login} />
+        </Switch>
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,59 @@
-import React from "react";
+import React, {useState} from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const Login = () => {
+
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const[credentials, setCred]= useState({username:'', password:''})
+
+
+  const handleChange = e => {
+    setCred({
+        ...credentials,
+        [e.target.name]: e.target.value
+    });
+  };
+
+
+    //makes request to api/login, sends object of username / password (this.state.credentials)
+  //gets token and saves in localstorage
+  const login = e => {
+    e.preventDefault();
+    // axiosWithAuth ==> ?? an axios instance; .post() ==> ?? promise
+    axiosWithAuth()
+      .post('/login', credentials)
+      .then(res => {
+          console.log(res.data)
+        localStorage.setItem('token', res.data.payload);
+        // redirect to the apps main page?
+        //Route component - props.history
+        props.history.push('/bubblepage');
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <div>
+        <form onSubmit={login}>
+          <input
+            type="text"
+            name="username"
+            value={credentials.username}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={credentials.password}
+            onChange={handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
+
     </>
   );
 };
